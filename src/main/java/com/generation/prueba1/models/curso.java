1fe2f6e49edd5629a668bd17bf34071126f64c7a
DaniGomez26 <danigp.93@gmail.com>
2023-03-01 21:25:06 -0300

    tercero

--- src/main/java/com/generation/prueba1/models/curso.java
+++ src/main/java/com/generation/prueba1/models/curso.java
@@ -1,5 +1,26 @@
 package com.generation.prueba1.models;
 
 public class curso {
+    public String alumnos;
+
+    public curso() {
+    }
+
+    public curso(String alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public String getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(String alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    @Override
+    public String toString() {
+        return "curso [alumnos=" + alumnos + "]";
+    }
     
 }
--- src/main/java/com/generation/prueba1/models/grado.java
+++ src/main/java/com/generation/prueba1/models/grado.java
@@ -1,5 +1,23 @@
 package com.generation.prueba1.models;
 
-public class grado {
+public class grado extends curso {
+    public Integer grado;
+
+    public grado() {
+        super();
+    }
+
+    public Integer getGrado() {
+        return grado;
+    }
+
+    public void setGrado(Integer grado) {
+        this.grado = grado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"grado [grado=" + grado + "]";
+    }
     
 }
