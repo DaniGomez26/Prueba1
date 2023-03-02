79db4de615ab261cae83b151ecb4c580717bcf20
DaniGomez26 <danigp.93@gmail.com>
2023-03-01 21:34:08 -0300

    cuarto

--- src/main/java/com/generation/prueba1/models/materia.java
+++ src/main/java/com/generation/prueba1/models/materia.java
@@ -1,5 +1,24 @@
 package com.generation.prueba1.models;
 
-public class materia {
+public class materia extends curso {
+    public Boolean materia;
+
+    public materia() {
+        super();
+    }
+    public materia(Boolean materia) {
+        this.materia = materia;
+    }
+
+    public materia(String alumnos, Boolean materia) {
+        super(alumnos);
+        this.materia = materia;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+"materia [materia=" + materia + "]";
+    }
+    
+
     
 }
